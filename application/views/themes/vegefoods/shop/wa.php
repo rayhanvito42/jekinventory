fc18984662afab95db9ab393bd5d294053579d2d
rayhanvito42 <89769183+rayhanvito42@users.noreply.github.com>
2023-07-15 10:39:23 +0700

    main

---
+++ application/views/themes/vegefoods/shop/wa.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Membuat Tombol Contact Us Whatsapp Pada Website</title>
+<style>
+   .tombol_contact_us{
+     position: fixed;right: 10px;bottom: 44px;
+   }
+   .tombol_contact_us img{
+     height: 44px;width: auto;
+   }
+</style>
+</head>
+<body>
+   <h1>Membuat Tombol Contact Us Whatsapp Pada Website</h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+   <p>Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet.</p>
+   <a href="https://wa.me/6283835741435?text=contoh%20isi%20pesan%20dikirim%20via%20whatsapp" class="tombol_contact_us"><img src="contact_us.png"></a>
+</body>
+</html>
